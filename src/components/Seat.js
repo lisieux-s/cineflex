17dbe22e8d65f58596dba4e65acf683b1f65a955
Sophia de Lisieux Zocoler <sophiazocoler@gmail.com>
2021-12-20 11:30:29 -0300

    small corrections

--- src/components/Seat.js
+++ src/components/Seat.js
@@ -2,34 +2,46 @@ import { useState, useEffect } from 'react';
 
 import styled from 'styled-components';
 
+
 export default function Seat(props) {
   const [isAvailable, setIsAvailable] = useState(true);
   const [isSelected, setIsSelected] = useState(false);
+  const [color, setColor] = useState('#C3CFD9');
+  const [border, setBorder] = useState('#7B8B99');
 
   if (props.isAvailable === false) {
     setIsAvailable(false);
+    setColor('#FBE192');
+    setBorder('#F7C52B');
   }
 
+  const Container = styled.button`
+    width: 26px;
+    height: 26px;
+    background: ${color};
+    border: 1px solid ${border};
+    box-sizing: border-box;
+    border-radius: 12px;
+    margin: 8px;
+  `;
+
   return (
-    <Container 
-    
-        onClick={() => {
-            if(isAvailable === true) {
-              isSelected ? setIsSelected(false) : setIsSelected(true); 
-            }
-            }
-        }>
-            {props.name}
+    <Container
+      onClick={(e) => {
+        e.stopPropagation();
+        if (isAvailable === true) {
+          isSelected ? setIsSelected(false) : setIsSelected(true);
+        } else {
+            alert("Esse assento não está disponível");
+        }
+        if (isSelected === true) {
+          setColor('#8DD7CF');
+          setBorder('#1AAE9E');
+        }
+      }}
+    >
+      {props.name}
     </Container>
   );
 }
 
-const Container = styled.button`
-  width: 26px;
-  height: 26px;
-  background: #c3cfd9;
-  border: 1px solid #808f9d;
-  box-sizing: border-box;
-  border-radius: 12px;
-  margin: 8px;
-`;
--- src/components/Success.js
+++ src/components/Success.js
@@ -1,3 +1,22 @@
-export default function Success() {
-    
+export default function Success(props) {
+  <div>
+    <p>Pedido feito com sucesso!</p>
+    <div>
+        <p><strong>Filme e sessão</strong></p>
+        <p>{props.movie.title}</p>
+        <p>{props.day.date} {props.name}</p>
+    </div>
+    <div>
+        <p><strong>Ingressos</strong></p>
+        {props.seats.map((seat) => <p>Assento {seat.name}</p>)}
+    </div>
+    <div>
+        <p><strong>Comprador</strong></p>
+        <p>Nome: {props.name}</p>
+        <p>CPF: {props.cpf}.replace(/(\d{3})(\d{3})(\d{3})(\d{2})/, "$1.$2.$3-$4")}`}</p>
+    </div>
+  </div>;
 }
+
+//ainda precisa ajustar as chamadas de props de acordo com 
+//o array construido em Seats
